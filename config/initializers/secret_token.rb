72f4bc0a8b026f4ad3fbbd7dab83e734824fb6af
Lucas Pizzagalli <lucas@zauberlabs.com>
2012-11-19 14:09:20 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgileWebRubyTutorial::Application.config.secret_token = '784d22fd7290517e53188ffacc144a81dccd2a546351d15988a95f390ebd55b64c3109919986e66576adcc6338c72a4307315778c71b33d45eed075042c353f9'
